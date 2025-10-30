944083e586fd954f747c262eaecadafc0f621500
Tony26112004 <s3974929@rmit.edu.vn>
2025-10-30 11:37:03 +0700

    Convert submodule to regular folder

---
+++ devops-directive-terraform-course/02-overview/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fa377108253bf620" # Ubuntu 20.04 LTS // ap-southeast-1
+  instance_type = "t2.micro"
+}
